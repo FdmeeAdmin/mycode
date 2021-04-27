429c54cc4de847ff12f3242db4d3b002774c3482
srinivasa <srinivasa.chittaluri@stellantis.com>
2021-04-27 15:29:43 +0000

    copying local files with python

---
+++ dicchallenge.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+char_name= {"Wolverine","Harry Potter", "Agent Fitz"}
+char_stat= {"real name", "powers", "archenemy" }
+heroes=  {
+"wolverine":
+    {"real name": "James Howlett",
+    "powers": "regeneration",
+    "archenemy": "Sabertooth",},
+"harry potter":
+    {"real name": "Harry Potter",
+    "powers": "magic",
+    "archenemy": "Voldemort",},
+"captain america":
+    {"real name": "Steve Rogers",
+    "powers": "frisbee shield",
+    "archenemy": "Hydra",}
+        }
+
+print(heroes["wolverine"])
+
+
+
