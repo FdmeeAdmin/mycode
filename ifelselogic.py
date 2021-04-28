decf6761b720b102f0d4792a27cec71b69e90336
srinivasa <srinivasa.chittaluri@stellantis.com>
2021-04-28 15:00:29 +0000

    studying for logic

---
+++ ifelselogic.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+choice={"Disney", "game of Thrones", "Harry Potter"}
+
---
+++ slicingcha.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+challenge= ["science", "turbo", ["goggles", "eyes"], "nothing"]
+
+print(challenge)
+
+print(+challenge[2])
+trial= ["science", "turbo", {"eyes": "goggles", "goggles": "eyes"}, "nothing"]
+
+nightmare= [{"slappy": "a", "text": "b", "kumquat": "goggles", "user":{"awesome": "c", "name": {"first": "eyes", "last": "toes"}},"banana": 15, "d": "nothing"}]
